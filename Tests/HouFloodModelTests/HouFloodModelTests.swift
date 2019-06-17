d8c09ac9fff9608413aa735d280fff3f8c1da15a
Aaron Farnham <aaron@tinyrobot.org>
2019-06-17 11:12:18 -0500

    Reduce min swift version to 5.0 from 5.1

--- Tests/HouFloodModelTests/HouFloodModelTests.swift
+++ Tests/HouFloodModelTests/HouFloodModelTests.swift
@@ -6,7 +6,7 @@ final class HouFloodModelTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(HouFloodModel().text, "Hello, World!")
+//        XCTAssertEqual(HouFloodModel().text, "Hello, World!")
     }
 
     static var allTests = [
